c7bd5516123b40cac110a301d2e747aa4eb0a876
qbgaoo <qbgaoo@126.com>
2024-10-04 21:29:41 +0800

    commit

---
+++ R/hello.R
@@ -0,0 +1,9 @@
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Cmd + Shift + B'
+#   Check Package:             'Cmd + Shift + E'
+#   Test Package:              'Cmd + Shift + T'
+
+hello <- function() {
+  print("Hello, world!")
+}
